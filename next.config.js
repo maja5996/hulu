47256a724689f2286837fe62374465c9ffd8609a
maja5996 <marijamihajlov2905@outlook.com>
2021-11-06 18:18:01 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["links.papareact.com", 'image.tmdb.org', 'assets.example.com'],
+    formats: ['image/avif', 'image/webp']
+
+  }
 }
